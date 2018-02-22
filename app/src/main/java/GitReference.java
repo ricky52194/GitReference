5c972ec95127a7c735985fc8d3b6602fa110b3c3
ricky52194 <ramosrs@g.cofc.edu>
2018-02-22 18:07:36 -0500

    Initial commit

---
+++ app/src/main/java/GitReference.java
@@ -0,0 +1,42 @@
+/**
+ * Created by ricky52194 on 2/22/18.
+ */
+
+public class GitReference {
+
+    private String command, example, explanation, section;
+
+    public GitReference(){}
+
+    public String getCommand() {
+        return command;
+    }
+
+    public void setCommand(String command) {
+        this.command = command;
+    }
+
+    public String getExample() {
+        return example;
+    }
+
+    public void setExample(String example) {
+        this.example = example;
+    }
+
+    public String getExplanation() {
+        return explanation;
+    }
+
+    public void setExplanation(String explanation) {
+        this.explanation = explanation;
+    }
+
+    public String getSection() {
+        return section;
+    }
+
+    public void setSection(String section) {
+        this.section = section;
+    }
+}
---
+++ app/src/main/java/com/example/ricky52194/gitreference/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.ricky52194.gitreference;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
